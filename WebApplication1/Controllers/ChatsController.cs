758642628bf1c0e98f52b7a00779e06a387fe76c
David Eggenberger <David.Eggenberger@advellence.com>
2023-09-15 23:44:21 +0200

    Add project files.

---
+++ WebApplication1/Controllers/ChatsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatsController : ControllerBase
+    {
+        public ChatsController()
+        {
+            
+        }
+
+        [HttpGet]
+        public async Task<bool> GetChat()
+        {
+            return true;
+        }
+    }
+}
